a6903b1ac18e42e04ba1732c39a859f703b60824
allinguaproject <schwarz.jakob.jan@zoho.eu>
2019-04-12 00:30:39 +0200

    ...

--- resources/views/_parts/nav_top.blade.php
+++ resources/views/_parts/nav_top.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-light ">
   
-  <a class="navbar-brand" id="LOGO_nav"> 
-  <img src="{{route('get.image','public\users\Lingmer_white.png')}}" alt="" width="18%" >
+  <a class="navbar-brand" >
+    <div  class="Logo_nav_white" ></div>
   </a>
 
   <ul class="navbar-nav">
-    <!--
+    
     <li class="nav-item active">
       <a class="lingo_nav_item" href="#">Home</a>
     </li>
     <li class="nav-item">
       <a class="lingo_nav_item" href="#">Über Uns</a>
     </li>
-    -->
+    
     
   </ul>
   <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
